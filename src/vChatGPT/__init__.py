5890fbebac467546a8f515bda1a9cc1d2804cceb
Yusuke Watanabe <wyusuuke@gmail.com>
2023-03-26 14:45:14 +0900

    v1.0

--- src/vChatGPT/__init__.py
+++ src/vChatGPT/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: "2023-03-26 13:25:53 (ywatanabe)"
+# Time-stamp: "2023-03-26 14:44:26 (ywatanabe)"
 
 __copyright__ = "Copyright (C) 2023 Yusuke Watanabe"
-__version__ = "0.12"
+__version__ = "1.0"
 __license__ = "GPL3.0"
 __author__ = "ywatanabe1989"
 __author_email__ = "ywata1989@gmail.com"
